3db6bdf58e1dbdf0354fe1df74292deacf7a114c
allthecodeDev <60193738+allthecodeDev@users.noreply.github.com>
2026-01-27 22:51:25 +0000

    feat(37-04): export FontDropdown from shared index
    - Add FontDropdown to shared property components exports
    - Enables clean imports like: import { FontDropdown } from './shared'

---
+++ src/components/Properties/shared/index.ts
@@ -0,0 +1,9 @@
+// Shared property components
+export { FontDropdown } from './FontDropdown'
+export { FontFamilySelect } from './FontFamilySelect'
+export { FontWeightSelect } from './FontWeightSelect'
+export { PositionSelect } from './PositionSelect'
+export { ColorsSection } from './ColorsSection'
+export { FontSection } from './FontSection'
+export { LabelDisplaySection } from './LabelDisplaySection'
+export { ValueDisplaySection } from './ValueDisplaySection'
